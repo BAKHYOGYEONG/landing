58bd5e180d05270cf58ed0b13c2e3cd67d7cbea8
BAKHYOGYEONG <vbn1525@gmail.com>
2022-12-07 14:15:12 +0900

    commit for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    port: 3000,
-    // proxy: {
-    //   '/api': { 
-    //     target: "sftp://vbn1424.cafe24.com",
-    //     changeOrigin: true,
-    //   }, 
-    // },
-  },
+  // devServer: {
+  //   port: 3000,
+  // },
 
 })
